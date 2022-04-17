f92a40de427519109f64af5262cf239aa39c2285
vlad9i2 <vlad9i2222@gmail.com>
2022-04-18 01:01:37 +0300

    Add Google auth. Add main page to deckbuild refernce.
    Simple auth with django-allauth added. Added link to login page. Now you
    can register with google account.

--- CCwebsite/CCwebsite/urls.py
+++ CCwebsite/CCwebsite/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib.auth.views import LogoutView
+
 urlpatterns = [
-    path('', include('DeckBuilder.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('logout', LogoutView.as_view()),
+    path('', include('social_django.urls', namespace='social')),
+    path('', include('DeckBuilder.urls')),
 ]
