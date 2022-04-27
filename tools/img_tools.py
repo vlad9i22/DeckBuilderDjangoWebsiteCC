82b2e3b3c58578a4e3dc68911236e607507af19d
Wrong_Wizzli <dima.vartan@gmail.com>
2022-04-27 17:20:40 +0300

    Improve deckbuilder page and fix images
    Main changes:
    * Bug with corrupted images fixed. Size of most of the images is smaller now for
      efficiency reasons
    * Added sort deck function to deckbuilder page. This will make interface show
      units in the sequence it is shown in the game
    * Added clear button to UI. Clear button clear all slots from units
    * Minor codestyle fixes according to flake8
    * Added sort order JSON static file

--- tools/img_tools.py
+++ tools/img_tools.py
@@ -1,40 +1,12 @@
 from PIL import Image
-import numpy as np
 from glob import glob
 from os import path
 from shutil import copytree, rmtree
 
 
-class Fix_Image:
-    """
-    Class helps to process images of units shown in data folder.
-    """
-
-    def __init__(self) -> None:
-        pass
-
-    def resize(self, img: Image, new_size: tuple) -> Image:
-        return img.resize(new_size)
-
-    def remove_blank(self, img: np.array) -> Image:
-        """
-        Removes blank white space around unit icon
-
-        Args:
-            img (np.array): Input image
-        Returns:
-            Image: PIL Image
-        """
-        h, w = img.shape[:2]
-        img = img.reshape(-1, 4)
-        img *= (img != [255, 255, 255, 0]).astype(np.uint8)
-        return Image.fromarray(img.reshape(h, w, 4))
-
-
 def get_all_file_names(dir_name: str) -> list:
     """
     Recursively gets all file names from given directory
-
     Args:
         dir_name (str): Directory name
     Returns:
@@ -53,7 +25,6 @@ def get_all_file_names(dir_name: str) -> list:
 def process_images(new_size: tuple) -> None:
     """
     Transforms raw image data to processed unit icons. REMOVES ./data directory
-
     Args:
         new_size (tuple): Size of cleaned images
     """
@@ -61,9 +32,9 @@ def process_images(new_size: tuple) -> None:
         rmtree("./data")
     copytree("./raw_data", "./data")
     file_names = get_all_file_names("./data/cards")
-    fixer = Fix_Image()
     for image_name in file_names:
-        img = np.asarray(Image.open(image_name))
-        img = fixer.remove_blank(img)
-        img = fixer.resize(img, new_size)
-        img.save(image_name)
+        Image.open(image_name).resize(new_size).save(image_name)
+
+
+if __name__ == "__main__":
+    process_images((136, 136))
